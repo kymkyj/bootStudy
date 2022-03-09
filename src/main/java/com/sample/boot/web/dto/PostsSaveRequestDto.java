64c33111ae0978c2b3efc0bc4659547703ffebb6
kymkyj <ymyj33@naver.com>
2022-03-10 01:12:23 +0900

    [feat] chap3, chap4 예제추가

---
+++ src/main/java/com/sample/boot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,25 @@
+package com.sample.boot.web.dto;
+
+import com.sample.boot.domain.posts.Posts;
+import lombok.Builder;
+
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}

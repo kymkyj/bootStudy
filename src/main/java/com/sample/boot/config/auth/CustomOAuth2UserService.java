277b5a10925f672cfd726e602768d9d7ec4e2a0e
kymkyj <ymyj33@naver.com>
2022-03-11 22:28:29 +0900

    [feat] 소셜로그인 1차 커밋

---
+++ src/main/java/com/sample/boot/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,56 @@
+package com.sample.boot.config.auth;
+
+import com.sample.boot.config.auth.dto.OAuthAttributes;
+import com.sample.boot.config.auth.dto.SessionUser;
+import com.sample.boot.domain.user.User;
+import com.sample.boot.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        // 현재 로그인 진행 중인 서비스 구분하는 코드
+        // 구글 네이버 카카오 등의 서비스 구분 코드
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName(); // 구글과 네이버를 동시 지원할 떄 구분
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+        User user = saveOrUpdate(attributes);
+
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+        }
+
+        private User saveOrUpdate(OAuthAttributes attributes) {
+            User user = userRepository.findByEmail(attributes.getEmail())
+                    .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                    .orElse(attributes.toEntity());
+
+            return userRepository.save(user);
+        }
+}
